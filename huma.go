cff7f67b0006376534b0f51d7d8a4a42b00aa11f
Lazhar <hello@lazharichir.com>
2025-07-30 21:08:13 +0100

    Add CloudEventHeaders struct to define event metadata (huma compatible)

---
+++ huma.go
@@ -0,0 +1,16 @@
+package events
+
+// As per https://cloud.google.com/eventarc/docs/cloudevents#http-request
+// ce-id			Unique identifier for the event	1096434104173400
+// ce-source		Identifies the source of the event	//pubsub.googleapis.com/projects/my-project/topics/my-topic
+// ce-specversion	The CloudEvents specification version used for this event	1.0
+// ce-type			The type of event data	google.cloud.pubsub.topic.v1.messagePublished
+// ce-time			Event generation time, in RFC 3339 format (optional)	2020-12-20T13:37:33.647Z
+
+type CloudEventHeaders struct {
+	ID          string  `header:"ce-id"`
+	Source      string  `header:"ce-source"`
+	SpecVersion string  `header:"ce-specversion"`
+	Type        string  `header:"ce-type"`
+	Time        *string `header:"ce-time,omitempty"` // Optional
+}
